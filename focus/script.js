06925833cc5dbd54405035f7efc5e58c027573b1
Myles Marino <MylesMarino1@gmail.com>
2022-11-19 20:03:21 -0500

    Add focus

---
+++ focus/script.js
@@ -0,0 +1,14 @@
+// getWeek
+	currentDate = new Date();
+	startDate = new Date(currentDate.getFullYear(), 0, 1);
+	var days = Math.floor((currentDate - startDate) /
+		(24 * 60 * 60 * 1000));
+	var weekNumber = Math.floor(days / 7);
+	var weekEven = weekNumber % 2 == 0 ? true : false
+
+// select elements
+	const left = document.getElementById("left");
+	const right = document.getElementById("right");
+
+// output
+	weekEven == true ? left.style.backgroundColor = "black" : right.style.backgroundColor = "black"
