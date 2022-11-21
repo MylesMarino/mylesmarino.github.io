fa8c54d53c0c6e5576743bbc46f0667e3b1a790d
Myles Marino <MylesMarino1@gmail.com>
2022-11-21 00:26:33 -0500

    autoCommit Mon 11/21/2022

--- focus/script.js
+++ focus/script.js
@@ -1,14 +1,20 @@
 // getWeek
-	currentDate = new Date();
-	startDate = new Date(currentDate.getFullYear(), 0, 1);
-	var days = Math.floor((currentDate - startDate) /
-		(24 * 60 * 60 * 1000));
-	var weekNumber = Math.floor(days / 7);
-	var weekEven = weekNumber % 2 == 0 ? true : false
+	// currentDate = new Date();
+	// startDate = new Date(currentDate.getFullYear(), 0, 2);
+	// var days = Math.floor((currentDate - startDate) /
+	// 	(24 * 60 * 60 * 1000)) ;
+	// var weekNumber = Math.floor(days / 7);
+	// var weekEven = weekNumber % 2 == 0 ? true : false
+
+	const date = new Date();
+	const numDate = date.getDate();
+	const numDay = date.getDay();
+	date.setDate(numDate - numDay);
+	const weekNum = Math.floor(date.getDate() / 7);
 
 // select elements
 	const left = document.getElementById("left");
 	const right = document.getElementById("right");
 
 // output
-	weekEven == true ? right.style.backgroundColor = "white" : life.style.backgroundColor = "white"
+	weekEven == true ? right.style.backgroundColor = "white" : left.style.backgroundColor = "white"
