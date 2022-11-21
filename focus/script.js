f5505b1050cd1c23f5fc6ff72b14da899efe2117
Myles Marino <MylesMarino1@gmail.com>
2022-11-21 00:31:06 -0500

    Add correct java

--- focus/script.js
+++ focus/script.js
@@ -1,14 +1,7 @@
 // getWeek
-	// currentDate = new Date();
-	// startDate = new Date(currentDate.getFullYear(), 0, 2);
-	// var days = Math.floor((currentDate - startDate) /
-	// 	(24 * 60 * 60 * 1000)) ;
-	// var weekNumber = Math.floor(days / 7);
-	// var weekEven = weekNumber % 2 == 0 ? true : false
-
 	const date = new Date();
 	const numDate = date.getDate();
-	const numDay = date.getDay();
+	var numDay = date.getDay();
 	date.setDate(numDate - numDay);
 	const weekNum = Math.floor(date.getDate() / 7);
 
@@ -17,4 +10,4 @@
 	const right = document.getElementById("right");
 
 // output
-	weekEven == true ? right.style.backgroundColor = "white" : left.style.backgroundColor = "white"
+	weekNum % 2 != 0 ? right.style.backgroundColor = "white" : left.style.backgroundColor = "white"
