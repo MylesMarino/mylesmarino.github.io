3ba506ac5904546f10459419ebcfbfbaa609fc6d
Myles Marino <MylesMarino1@gmail.com>
2022-11-27 21:56:41 -0500

    autoCommit Sun 11/27/2022

--- focus/script.js
+++ focus/script.js
@@ -1,13 +1,19 @@
 // getWeek
-	const date = new Date();
-	const numDate = date.getDate();
-	var numDay = date.getDay();
-	date.setDate(numDate - numDay + 1);
-	const weekNum = Math.floor(date.getDate() / 7);
+const date = new Date();
+const dateNum = date.getDate();
+console.log(dateNum);
 
-// select elements
-	const left = document.getElementById("left");
-	const right = document.getElementById("right");
 
-// output
-	weekNum % 2 != 0 ? right.style.backgroundColor = "white" : left.style.backgroundColor = "white"
+// // getWeek
+// 	const date = new Date();
+// 	const numDate = date.getDate();
+// 	var numDay = date.getDay();
+// 	date.setDate(numDate - numDay + 1);
+// 	const weekNum = Math.floor(date.getDate() / 7);
+
+// // select elements
+// 	const left = document.getElementById("left");
+// 	const right = document.getElementById("right");
+
+// // output
+// 	weekNum % 2 != 0 ? right.style.backgroundColor = "white" : left.style.backgroundColor = "white"
