af17b01096be7ad54a169bb2d0ab2669de85805f
Myles Marino <MylesMarino1@gmail.com>
2022-11-28 08:56:30 -0500

    autoCommit Mon 11/28/2022

--- focus/script.js
+++ focus/script.js
@@ -1,10 +1,9 @@
 // getWeek
 	let currentDate = new Date();
-	let startDate = new Date(currentDate.getFullYear(),0,1);
+	let startDate = new Date(2022,0,2);
 
 	let dayDifference = Math.floor(currentDate - startDate)/(1000*60*60*24);
 	let weekNum = Math.ceil((dayDifference)/7);
-	console.log(weekNum)
 
 // output
 	weekNum % 2 != 0 ? document.getElementById("whatWeek").innerHTML = "Code." : document.getElementById("whatWeek").innerHTML = "Invest."
